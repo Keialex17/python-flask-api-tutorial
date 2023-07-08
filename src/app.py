ff41182c93a4de4e0be999cb018b2072c0537269
Keialex17 <115053607+Keialex17@users.noreply.github.com>
2023-07-08 21:10:48 +0000

    listo :white_check_mark:

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+todos = [{
+  "label": "My first task", "done": False
+}]
+
+
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return '<h1>Hello!</h1>'
+  json_text = jsonify(todos)
+  return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    # json_text = jsonify(todos)
+    todos.append(request_body)
+    return todos
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    json_text = jsonify(todos)
+    return json_text
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
